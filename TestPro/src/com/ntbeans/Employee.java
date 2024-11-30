361e254380e0c62501c3633227d4bf9fc7910dfe
hemasreep <HemasreePedi_jccm80t@LAPTOP-HEMASHREE>
2024-11-30 20:12:06 +0530

    second commit by tl

---
+++ TestPro/src/com/ntbeans/Employee.java
@@ -0,0 +1,28 @@
+package com.ntbeans;
+
+public class Employee {
+	
+	String ename;
+	
+	String eno;
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getEno() {
+		return eno;
+	}
+
+	public void setEno(String eno) {
+		this.eno = eno;
+	}
+	
+	
+	
+
+}
